ff9714c3cabf35a10de812a9f6dd3e841a812ffd
Santiago Perez Perret <perezperret@gmail.com>
2018-10-05 18:38:17 -0400

    Add pagination to Institutions::Index

--- src/actions/institutions/index.cr
+++ src/actions/institutions/index.cr
@@ -4,6 +4,8 @@ class Institutions::Index < ApiAction
 
   route do
     institutions = InstitutionQuery.new
+      .limit(per_page)
+      .offset((page - 1) * per_page)
     total = InstitutionQuery.new.count
 
     json Institutions::IndexSerializer.new(institutions, total, page, per_page)
