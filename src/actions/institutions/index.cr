79f722a04652ac71220ebf2bc3aab1ebe972ff5f
Santiago Perez Perret <perezperret@gmail.com>
2018-10-05 19:02:44 -0400

    Add name search

--- src/actions/institutions/index.cr
+++ src/actions/institutions/index.cr
@@ -1,11 +1,18 @@
 class Institutions::Index < ApiAction
   param page : Int32 = 1
   param per_page : Int32 = 20
+  param search : String?
 
   route do
     institutions = InstitutionQuery.new
       .limit(per_page)
       .offset((page - 1) * per_page)
+
+    unless search.nil? || search.try(&.empty?)
+      puts search
+      institutions = institutions.name.ilike("%#{search}%")
+    end
+
     total = InstitutionQuery.new.count
 
     json Institutions::IndexSerializer.new(institutions, total, page, per_page)
